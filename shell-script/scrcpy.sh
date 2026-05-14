5c7ce469e3010c29b6992961100231e32c2e6725
balibabu <balibabuchauhan1@gmail.com>
2026-05-14 19:43:25 +0545

    added some extra options to scrcpy

--- shell-script/scrcpy.sh
+++ shell-script/scrcpy.sh
@@ -1,5 +1,5 @@
 dns=$(nmcli -g IP4.DNS dev show wlp5s0 | head -n1)
-adb connect "${dns}:5555"
+adb connect "${dns}:5445"
 cd /home/lappy/Documents
-scrcpy -S -K > scrcpy-log.txt &
+scrcpy -S -K --max-fps 24 --no-mouse-hover --no-power-on --window-height 1080 --window-borderless --window-x=1920 > scrcpy-log.txt &
 read -t 3
